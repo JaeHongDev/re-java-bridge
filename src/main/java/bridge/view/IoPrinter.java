a26c0a24215a43fcbaa7df85670d635a554415fd
JaehongDev <println@kakao.com>
2022-12-12 20:51:20 +0900

    feature (IoPrinter) : 기본 출력 메서드 구현

--- src/main/java/bridge/view/IoPrinter.java
+++ src/main/java/bridge/view/IoPrinter.java
@@ -1,4 +1,21 @@
 package bridge.view;
 
 public class IoPrinter {
+    protected void println(final Object message) {
+        System.out.println(message);
+    }
+
+    protected void println() {
+        this.println("");
+    }
+
+    protected void printlnAndAfterNewLine(final Object message) {
+        this.println(message);
+        this.println();
+    }
+
+    protected void printlnByFormat(final String format, final Object... args) {
+        System.out.printf(format, args);
+    }
+
 }
