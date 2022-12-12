97ff59066c5116891db6b5f657813ae773975459
JaehongDev <println@kakao.com>
2022-12-12 20:45:02 +0900

    feature (BridgeGameStatus) : 다리 건너기게임의 상태를 전체상태에서 가지고 오도록 구현

--- src/main/java/bridge/launcher/status/BridgeGameStatus.java
+++ src/main/java/bridge/launcher/status/BridgeGameStatus.java
@@ -1,18 +1,21 @@
 package bridge.launcher.status;
 
 import bridge.launcher.context.BridgeGameContext;
+import bridge.launcher.context.SystemContext;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public abstract class BridgeGameStatus implements BasicStatus {
+    protected final SystemContext context;
     protected final InputView inputView;
     protected final OutputView outputView;
-    protected final BridgeGameContext context;
+    protected final BridgeGameContext bridgeGameContext;
 
-    public BridgeGameStatus(final BridgeGameContext context) {
-        this.context = context;
-        this.inputView = context.getInputView();
-        this.outputView = context.getOutputView();
+    public BridgeGameStatus(final SystemContext systemContext) {
+        this.context = systemContext;
+        this.bridgeGameContext = systemContext.getBridgeGameContext();
+        this.inputView = systemContext.getInputView();
+        this.outputView = systemContext.getOutputView();
     }
 
 }
