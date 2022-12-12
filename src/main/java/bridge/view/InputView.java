82c50c3f0905d2c9b61ca1ddecaa2baf4689c4cc
JaehongDev <println@kakao.com>
2022-12-12 20:53:31 +0900

    feature (InputView) : 입력 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,15 +1,12 @@
 package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-public class InputView {
-
-    public InputView() {
-        System.out.println("생성되었습니다.");
-    }
+public class InputView extends IoPrinter {
 
     /**
      * 다리의 길이를 입력받는다.
@@ -31,5 +28,19 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private String readLine() {
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException exception) {
+            throw new IllegalStateException("더이상 입력할 수 없습니다.");
+        }
+    }
+
+    private String readLineAndAfterNewLine() {
+        var input = readLine();
+        this.println();
+        return input;
     }
 }
